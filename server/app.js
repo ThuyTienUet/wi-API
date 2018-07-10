fa97b6e22a082850943a1b5cfcf0fc11a7fb1a82
ThuyTienUet <tien280198@gmail.com>
2018-07-10 18:39:21 +0700

    update

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ mongoose.connection.on("error", function (err) {
     return console.log(err);
 });
 
-mongoURI = 'mongodb://localhost:27017/api-well-insight';
+mongoURI = 'mongodb://localhost:27017/apiWI';
 
 mongoose.connect(mongoURI);
 // mongoose.connect(
