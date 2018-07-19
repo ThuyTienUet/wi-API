e33a34e12b540b364219d64faa783f4bc28943d5
ThuyTienUet <tien280198@gmail.com>
2018-07-19 15:50:52 +0700

    update

---
+++ demo/src/apiSOM.js
@@ -0,0 +1,33 @@
+/**
+ * @fileoverview Module
+ */
+/**
+ * @module API_SOM
+ */
+/**
+ * @param {String} model_id
+ * @param {2D_array} data
+ * @implements Method: POST
+ * @implements Route: /api/v1.0/lvq/predict
+ * @implements Note: 
+ * @example Example Response: 
+{
+}
+ * @example Example Payload: 
+{
+	"model_id" : "model-1", 
+	"data" : {
+		"0" : {
+			"0" : 1, 
+			"1" : 2, 
+			"2" : 3
+ }, 
+		"1" : {
+			"0" : 2, 
+			"1" : 3, 
+			"2" : 4
+		}
+	}
+}
+ */
+function Predicting_the_result_using_trained_model(model_id, data) {}
